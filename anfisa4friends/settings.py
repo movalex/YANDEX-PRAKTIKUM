1a1a113c3111b67626acec93113f385dde1d42d3
Alex Bogomolov <mail@abogomolov.com>
2021-03-27 15:05:35 +0300

    update models

--- anfisa4friends/settings.py
+++ anfisa4friends/settings.py
@@ -58,7 +58,10 @@ DATABASES = {
     }
 }
 
+ROOT_URLCONF = 'anfisa4friends.urls'
+
 # Password validation
+
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
--- icecream/views.py
+++ icecream/views.py
@@ -3,8 +3,7 @@ from .models import icecream_db
 
 
 def icecream_list(request):
-    # Получаем данные: полный список сортов мороженого
     db_query = icecream_db
-    icecreams = f'Список мороженого: {db_query}'
-    # Возвращаем результат в браузер пользователя
+    icecream_list = [i['name'] for i in db_query]
+    icecreams = f'Список сортов мороженого: {" :: ".join(icecream_list)}'
     return HttpResponse(icecreams)
