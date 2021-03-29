8dc07f7e8ecaf2e0b5427c8694257988aee14e82
Alexey Bogomolov <mail@abogomolov.com>
2021-03-29 16:12:15 +0300

    praktikum context validator bug

--- icecream/views.py
+++ icecream/views.py
@@ -1,9 +1,21 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import icecream_db
 
 
-def icecream_list(request):
+# def icecream_list(request): # этот вариант не проходит проверку
+#     context = {}
+#     icecreams = ""
+#     for i in range(len(icecream_db)):
+#         icecreams += icecream_db[i]['name'] + "<br>"
+#     context['icecreams'] = icecreams
+#
+#     return render(request, 'icecream/icecream-list.html', context=context)
+
+
+def icecream_list(request):  # этот проходит
     icecreams = ''
     for i in range(len(icecream_db)):
-        icecreams += f"{icecream_db[i]['name']} :: "
-    return HttpResponse(f'Cписок сортов мороженого: {icecreams}')
+        icecreams += f"{icecream_db[i]['name']}<br>"
+        context = {'icecreams': icecreams}
+
+    return render(request, 'icecream/icecream-list.html', context)
