ed388207c4893dfc96e716572d791b94a0662929
Alexey Bogomolov <mail@abogomolov.com>
2021-03-30 18:59:08 +0300

    lesson 3.3 solve

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from icecream.models import icecream_db
 from anfisa.models import friends_db
-# Импорт должен быть тут
+from anfisa.services import what_weather
 
 
 def index(request):
@@ -9,13 +9,9 @@ def index(request):
     friends = ''
 
     for friend in friends_db:
-        # Узнайте город друга и сохраните его в city
-        city = ...
-        # Получите погоду из функции what_weather() и сохраните её в переменную weather
-        weather = ...
-        # Здесь через f-строку объедините переменные в одну строчку:
-        # имя :: город :: погода
-        friends += f'{friend} <br>'
+        city = friends_db[friend]
+        weather = what_weather(city)
+        friends += f'{friend} :: {city} ::{weather}<br>'
 
     for i in range(len(icecream_db)):
         icecreams += (f'{icecream_db[i]["name"]} | '
