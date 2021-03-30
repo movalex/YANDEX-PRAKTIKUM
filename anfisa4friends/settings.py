ee32407291328e25772c0aa76839be2d36fbce76
Alexey Bogomolov <mail@abogomolov.com>
2021-03-30 17:15:15 +0300

    lesson 02

--- anfisa4friends/settings.py
+++ anfisa4friends/settings.py
@@ -65,5 +65,4 @@ LANGUAGE_CODE = 'ru'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
--- homepage/views.py
+++ homepage/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from icecream.models import icecream_db
+# Здесь импортируйте словарь friends_db из файла anfisa/models.py
 
 
 def index(request):
     icecreams = ''
+    # Создайте пустую переменную friends 
+    # Циклом обойдите словарь friends_db и сохраните все имена друзей в переменную friends
     for i in range(len(icecream_db)):
-        icecreams += f"{icecream_db[i]['name']} | <a href='/icecream/{i}/'>Узнать состав</a><br>"
+        icecreams += (f'{icecream_db[i]["name"]} |' 
+                    f'<a href="icecream/{i}/">  Узнать состав</a><br>')
     context = {
         'icecreams': icecreams,
+        # Добавьте переменную friends в словарь context
     }
     return render(request, 'homepage/index.html', context)
