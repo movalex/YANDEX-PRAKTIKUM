f886e0f3af6272e3099d90b46d03c267f6a48319
Alexey Bogomolov <mail@abogomolov.com>
2021-03-31 07:43:13 +0300

    lesson 4.3 solve

--- homepage/views.py
+++ homepage/views.py
@@ -1,43 +1,45 @@
-icecreams = ''
-friends = ''
-city_weather = ''
-friend_output = ''
-# Добавлена пустая  переменная для хранения сорта мороженого, полученного из запроса
-selected_icecream = ''
+from django.shortcuts import render
+from icecream.models import icecream_db
+from anfisa.models import friends_db
+from anfisa.services import what_weather
 
-for friend in friends_db:
-    # Около каждого имени вставляется radio button,
-    # и теперь в форме кликом по кнопочке можно будет выбрать одного из друзей.
-    friends += (f'<input type="radio" name="friend"'
-                f' required value="{friend}">{friend}<br>')
 
-for i in range(len(icecream_db)):
-    # В переменную ice_form добавьте HTML-код радио-кнопки и название мороженого
-    # (за образец можно взять код для списка друзей)
-    ice_form = ''
+def index(request):
+    icecreams = ''
+    friends = ''
+    city_weather = ''
+    friend_output = ''
+    # Добавлена пустая  переменная для хранения сорта мороженого, полученного из запроса
+    selected_icecream = ''
 
-    ice_link = f'<a href="icecream/{i}/">Узнать состав</a>'
-    icecreams += f'{ice_form} | {ice_link} <br>'
+    for friend in friends_db:
+        # Около каждого имени вставляется radio button,
+        # и теперь в форме кликом по кнопочке можно будет выбрать одного из друзей.
 
-if request.method == 'POST':
-    # Извлекли из запроса имя друга
-    selected_friend = request.POST['friend']
-    # Сохраните  в переменную selected_icecream название мороженого, полученное в POST-запросе
-    selected_icecream = ...
+        friends += (f'<input type="radio" name="friend"'
+                    f' required value="{friend}">{friend}<br>')
 
-    # В переменную city записано название города
-    city = friends_db[selected_friend]
-    # Запрос погоды в городе city
-    weather = what_weather(city)
+    for i in range(len(icecream_db)):
+        # В переменную ice_form добавьте HTML-код радио-кнопки и название мороженого
+        # (за образец можно взять код для списка друзей)
+        icecream_name = icecream_db[i]["name"]
+        ice_form = f'<input type="radio" name="icecream" required value="{icecream_name}">{icecream_name}'
+        ice_link = f'<a href="icecream/{i}/">Узнать состав</a>'
+        icecreams += f'{ice_form} | {ice_link} <br>'
 
-    # Вместо слова "мороженое" выведите название сорта из запроса.
-    friend_output = f'{selected_friend}, тебе прислали мороженое!'
-    city_weather = f'В городе {city} погода: {weather}'
+    if request.method == 'POST':
+        selected_friend = request.POST['friend']
+        selected_icecream = request.POST['icecream']
+        city = friends_db[selected_friend]
+        weather = what_weather(city)
+        # Вместо слова "мороженое" выведите название сорта из запроса.
+        friend_output = f'{selected_friend}, тебе прислали {selected_icecream}!'
+        city_weather = f'Погода в городе {city}: {weather}'
 
-context = {
-    'icecreams': icecreams,
-    'friends': friends,
-    'friend_output': friend_output,
-    'city_weather': city_weather,
-}
-return render(request, 'homepage/index.html', context)
+    context = {
+        'icecreams': icecreams,
+        'friends': friends,
+        'friend_output': friend_output,
+        'city_weather': city_weather,
+    }
+    return render(request, 'homepage/index.html', context)
